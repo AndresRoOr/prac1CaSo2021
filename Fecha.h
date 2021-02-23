016a838a55dc2695fc914d1f1dfb830aeeededf6
Andrés Rojas Ortega <aro00015@red.ujaen.es>
2021-02-23 19:29:17 +0100

    Fecha.h: Añadidas etiquetas doxygen (COMPLETAR)

--- Fecha.h
+++ Fecha.h
@@ -1,108 +1,207 @@
+/**
+ * @file	Fecha.h
+ * @brief	Archivo cabecera donde se almacena toda la información relacionada con la clase Fecha
+ */
+
 #pragma once
 #ifndef FECHA_H
 #define FECHA_H
 #include <string>
 using namespace std;
 
-/** @brief Excepción que representa fechas incorrectas */
+/**
+ * @brief Excepción que representa fechas incorrectas
+ */
 class ErrorFechaIncorrecta {
 };
 
-/** @brief Clases sencilla para representar fechas y horas */
+/**
+ * @brief Clase sencilla para representar fechas y horas
+ */
 class Fecha {
-    /** Fecha y hora */
-    unsigned dia, mes, anio, hora, min;
-    /** Dias por mes */
-    static const unsigned diasMes[12];
+	unsigned dia, mes, anio, hora, min; ///< Información de fecha y hora
+	static const unsigned diasMes[12]; ///< Almacena los días por mes
 
-    /** Comprobación de validez de una fecha */
-    void comprobarFecha(unsigned aDia, unsigned aMes, unsigned aAnio, unsigned aHora, unsigned aMin) const;
-    /** Función auxiliar de conversión desde estructura de tiempo tm de time.h */
-    void leerTiempo(const tm &t);
-    /** Función	auxiliar de conversión a estructura de tiempo tm de time.h */
-    void escribirTiempo(tm &t);
+	/**
+	 * @brief Comprobación de la validez de una fecha
+	 */
+	void comprobarFecha(unsigned aDia, unsigned aMes, unsigned aAnio,
+			unsigned aHora, unsigned aMin) const;
+
+	/**
+	 * @brief Función auxiliar de conversión desde estructura de tiempo tm de time.h
+	 */
+	void leerTiempo(const tm &t);
+
+	/**
+	 * @brief Función auxiliar de conversión a estructura de tiempo tm de time.h
+	 */
+	void escribirTiempo(tm &t);
 
 public:
-    /** Crea una fecha con la hora actual */
-    Fecha();
-    /** Crea una fecha concreta. Devuelve una excepción ErrorFechaIncorrecta si la fecha introducida no es correcta */
-    Fecha(unsigned aDia, unsigned aMes, unsigned aAnio, unsigned aHora = 0, unsigned aMin = 0);
 
-    /** Constructor copia */
-    Fecha(const Fecha &f) :
-    dia(f.dia), mes(f.mes), anio(f.anio), hora(f.hora), min(f.min) {
-    }
+	/**
+	 * @brief Constructor por defecto de la clase Fecha
+	 *
+	 * Crea una fecha con la hora actual
+	 */
+	Fecha();
 
-    /** Asignar un nuevo día */
-    void asignarDia(unsigned aDia, unsigned aMes, unsigned aAnio);
-    /** Asignar una nueva hora */
-    void asignarHora(unsigned aHora, unsigned aMin);
+	/**
+	 * @brief Constructor parametrizado de la clase Fecha
+	 *
+	 * Crea una fecha concreta. Devuelve una excepción ErrorFechaIncorrecta si la fecha introducida no es correcta
+	 */
+	Fecha(unsigned aDia, unsigned aMes, unsigned aAnio, unsigned aHora = 0,
+			unsigned aMin = 0);
 
-    /** Obtener hora */
-    unsigned verHora() const {
-        return hora;
-    }
+	/**
+	 * @brief Constructor por copia de la clase Fecha
+	 */
+	Fecha(const Fecha &f) :
+			dia(f.dia), mes(f.mes), anio(f.anio), hora(f.hora), min(f.min) {
+	}
 
-    /** Obtener minutos */
-    unsigned verMin() const {
-        return min;
-    }
+	/**
+	 * @brief Asigna un nuevo día
+	 * @param [in] aDia unsigned.
+	 * @param [in] aMes unsigned.
+	 * @param [in] aAnio unsigned.
+	 */
+	void asignarDia(unsigned aDia, unsigned aMes, unsigned aAnio);
 
-    /** Obtener dia */
-    unsigned verDia() const {
-        return dia;
-    }
+	/**
+	 * @brief Asigna una nueva hora
+	 * @param [in] aHora unsigned.
+	 * @param [in] aMin unsigned.
+	 */
+	void asignarHora(unsigned aHora, unsigned aMin);
 
-    /** Obtener mes */
-    unsigned verMes() const {
-        return mes;
-    }
+	/**
+	 * @brief Devuelve la hora
+	 * @return hora unsigned.
+	 */
+	unsigned verHora() const {
+		return hora;
+	}
 
-    /** Obtener año */
-    unsigned verAnio() const {
-        return anio;
-    }
+	/**
+	 * @brief Obtener minutos
+	 * @return min unsigned.
+	 */
+	unsigned verMin() const {
+		return min;
+	}
 
-    /** Obtener una cadena con el día */
-    string cadenaDia() const;
-    /** Obtener una cadena con la hora */
-    string cadenaHora() const;
+	/**
+	 * @brief Obtener dia
+	 * @return dia unsigned.
+	 */
+	unsigned verDia() const {
+		return dia;
+	}
 
-    /** Obtener una cadena con día y hora */
-    string cadena() const {
-        return cadenaDia() + " " + cadenaHora();
-    }
+	/**
+	 * @brief Obtener mes
+	 * @return mes unsigned.
+	 */
+	unsigned verMes() const {
+		return mes;
+	}
 
-    /** Indica si las dos fechas tienen el mismo día */
-    bool mismoDia(const Fecha &f) const {
-        return dia == f.dia && mes == f.mes && anio == f.anio;
-    }
-    /** Comparar fechas */
-    bool operator<(const Fecha &f);
-    /** Asignar fechas */
-    Fecha &operator=(const Fecha &f);
+	/**
+	 * @brief Obtener año
+	 * @return anio unsigned
+	 */
+	unsigned verAnio() const {
+		return anio;
+	}
 
-    /** Añadir un número de minutos indicado */
-    void anadirMin(int numMin);
-    
-    /** Añadir un número de horas indicado*/
-    void anadirHoras(int numHoras);
-    
-    /** Añadir un número de días indicado */
-    void anadirDias(int numDias);
-    
-    /** Añadir un número de meses indicado */
-    void anadirMeses(int numMeses);
-    
-    /** Añadir un número de años indicado */
-    void anadirAnios(int numAnios);
+	/**
+	 * @brief Obtener una cadena con el día
+	 * @return
+	 */
+	string cadenaDia() const;
 
-    ~Fecha();
+	/**
+	 * @brief Obtener una cadena con la hora
+	 * @return
+	 */
+	string cadenaHora() const;
+
+	/**
+	 * @brief Obtener una cadena con día y hora
+	 * @return
+	 */
+	string cadena() const {
+		return cadenaDia() + " " + cadenaHora();
+	}
+
+	/**
+	 * @brief Indica si las dos fechas tienen el mismo día
+	 * @param [in] f Fecha(dir, const).
+	 * @return
+	 */
+	bool mismoDia(const Fecha &f) const {
+		return dia == f.dia && mes == f.mes && anio == f.anio;
+	}
+
+	/**
+	 * @brief Comparar fechas
+	 * @param [in] f Fecha(dir, const).
+	 * @return
+	 */
+	bool operator<(const Fecha &f);
+
+	/**
+	 * @brief Operador de asignación de la clase Fecha
+	 * @param [in] f Fehca(const, dir)
+	 * @return Devuelve una nueva instancia de la clase Fecha con los datos de la Fecha pasada como parámetro.
+	 */
+	Fecha &operator=(const Fecha &f);
+
+	/**
+	 * @brief Añadir un número de minutos indicado
+	 * @param [in] numMin int.
+	 */
+	void anadirMin(int numMin);
+
+	/**
+	 * @brief Añadir un número de horas indicado
+	 * @param [in] numHoras int.
+	 */
+	void anadirHoras(int numHoras);
+
+	/**
+	 * @brief Añadir un número de días indicado
+	 * @param [in] numDias int.
+	 */
+	void anadirDias(int numDias);
+
+	/**
+	 * @brief Añadir un número de meses indicado
+	 * @param [in] numMeses
+	 */
+	void anadirMeses(int numMeses);
+
+	/**
+	 * @brief Añadir un número de años indicado
+	 * @param [in] numAnios int.
+	 */
+	void anadirAnios(int numAnios);
+
+	/**
+	 * @brief Destructor de la clase Fecha
+	 */
+	~Fecha();
 };
 
-/** Serialización de fechas */
+/**
+ * @brief Serialización de fechas
+ * @param os ostream(dir).
+ * @param f Fecha(const, dir).
+ * @return
+ */
 ostream &operator<<(ostream &os, const Fecha &f);
 
 #endif
-
-
