686dd4fdc88d91e4abe8124b15819558020f9b58
Andrés Rojas Ortega <aro00015@red.ujaen.es>
2021-02-24 14:34:58 +0100

    Libro.h: Añadidas etiquetas doxygen (COMPLETAR)

--- Libro.h
+++ Libro.h
@@ -1,3 +1,8 @@
+/**
+ * @file Libro.h
+ * @brief Archivo cabecera donde se almacena toda la información relacionada con la clase Libro
+ */
+
 #ifndef LIBRO_H
 #define	LIBRO_H
 
@@ -5,15 +10,40 @@
 #include "lista_sin.h"
 using namespace std;
 
+/**
+ * @brief Clase que representa toda la información necesaria de un libro
+ */
 class Libro {
-    string titulo, autores, editorial, ISBN;
-    int anio;
-    float precioActual;
+
+    string titulo;///< Título del libro
+    string autores;///< Autor/es del libro
+    string editorial;///< Editorial que publicó el libro
+    string ISBN;///< Código ISBN identificativo del libro
+    int anio;///< Año en el que se publicó el libro
+    float precioActual;///< Precio actual del libro
 
 public:
+
+    /**
+     * @brief Constructor por defecto de la clase Libro
+     */
     Libro();
+
+    /**
+     * @brief Constructor parametrizado de la clase Libro
+     * @param [in] aTitulo string. Título del libro
+     * @param [in] aAutores string. Autor/es del libro
+     * @param [in] aEditorial string. Editorial del libro
+     * @param [in] aISBN string. ISBN del libro
+     * @param [in] aAnio int. Año de publicación del libro
+     * @param [in] aPrecioActual float. Precio del libro
+     */
     Libro(string aTitulo, string aAutores, string aEditorial, string aISBN, int aAnio, float aPrecioActual);
 
+    /**
+     * @brief Constructor por copia de la clase Libro
+     * @param [in] lib Libro(dir). Instancia de la clase Libro de la cual se va realizar una copia
+     */
     Libro(const Libro &lib) {
         this->titulo = lib.titulo;
         this->autores = lib.autores;
@@ -21,6 +51,11 @@ public:
         this->ISBN = lib.ISBN;
     }
 
+    /**
+     * @brief Operador de asignación de la clase Libro
+     * @param [in] lib Libro(dir). Instancia de la clase Libro de la cual se va a realizar una copia
+     * @return Devuelve una nueva instancia de la clase Libro con la información de lib
+     */
     Libro& operator=(const Libro &lib) {
         this->titulo = lib.titulo;
         this->autores = lib.autores;
@@ -29,22 +64,54 @@ public:
         return *this;
     }
 
+    /**
+     * @brief Operador menor que de la clase Libro
+     * @param [in] lib(dir, const). Instancia de la clase Libro con la que queremos realizar la comparación.
+     * @return true en el caso de que el ISBN de la instancia actual sea menor que el de la instancia pasada como parámetro
+     */
     bool operator<(const Libro &lib) {
         return (this->ISBN < lib.ISBN);
     }
+
+    /**
+     * @brief Operador de stream de la clase Libro
+     * @param
+     * @param
+     * @return
+     */
     friend ostream& operator<<(ostream&, const Libro&);
     string daISBN() {
         return ISBN;
     }
+
+    /**
+     * @brief
+     * @param [in] libro Libro(ref).
+     * @return
+     */
     string daTitulo(Libro *libro) {
         return titulo;
     }
+
+    /**
+     * @brief
+     * @return
+     */
     string daTitulo() {
         return titulo;
     }
+
+    /**
+     * @brief
+     * @return
+     */
     float daPrecioActual() {
         return precioActual;
     }
+
+    /**
+     * @brief Destructor de la clase Libro
+     */
     ~Libro();
 
 };
