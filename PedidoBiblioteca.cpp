078ac3853360a9a5648bfc11d43649d609bac3c7
Andrés Rojas Ortega <aro00015@red.ujaen.es>
2021-03-02 19:36:33 +0100

    PedidoBiblioteca.cpp: Añadidas etiquetas doxygen

--- PedidoBiblioteca.cpp
+++ PedidoBiblioteca.cpp
@@ -1,26 +1,32 @@
+/**
+ * @file PedidoBiblioteca.cpp
+ * @brief Contiene el código fuente de todas las funciones del archivo cabecera PedidoBiblioteca.h
+ */
+
 #include "PedidoBiblioteca.h"
 
-PedidoBiblioteca::PedidoBiblioteca() : fecha() {
-    importe = 0;
-    tramitado = false;
-    this->pedido_usu = pedido_usu;
+PedidoBiblioteca::PedidoBiblioteca() :
+		fecha() {
+	importe = 0;
+	tramitado = false;
+	this->pedido_usu = pedido_usu;
 }
 
 void PedidoBiblioteca::insertaPedidoLibro(PedidoUsuario *pedidoUsuario) {
-    this->pedido_usu.aumenta(pedidoUsuario);
-    float pre = pedidoUsuario->daPrecio();
-    this->importe = this->importe + pre;
+	this->pedido_usu.aumenta(pedidoUsuario);
+	float pre = pedidoUsuario->daPrecio();
+	this->importe = this->importe + pre;
 }
 
 ostream& operator<<(ostream& co, PedidoBiblioteca& pb) {
-    co << "Fecha: " << pb.fecha << endl << "Importes: " << pb.importe << endl << "Tramitado: " << pb.tramitado << endl << "Tamanio: " << pb.pedido_usu.tamanio() << endl;
-    return co;
+	co << "Fecha: " << pb.fecha << endl << "Importes: " << pb.importe << endl
+			<< "Tramitado: " << pb.tramitado << endl << "Tamanio: "
+			<< pb.pedido_usu.tamanio() << endl;
+	return co;
 }
 
 void PedidoBiblioteca::cierraPedido() {
-    tramitado = true;
+	tramitado = true;
 
 }
-
-
 
