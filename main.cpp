5981e92f364302666a69cce1703e801e973045d8
Andrés Rojas Ortega <aro00015@red.ujaen.es>
2021-03-02 19:09:27 +0100

    main.cpp: Añadidas etiquetas doxygen

--- main.cpp
+++ main.cpp
@@ -1,31 +1,37 @@
+/**
+ * @file main.cpp
+ * @brief Archivo principal de la aplicaci贸n
+ */
 #include <cstdlib>
 #include "Aplication.h"
 
 using namespace std;
 
+/**
+ * @brief Funci贸n principal de la aplicaci贸n
+ * @return 0 en caso de ejecuci贸n satisfactoria, otro valor en cualquier otro caso.
+ */
 int main() {
-    unsigned opcion;
-    Aplication aplica;
-    do {
-        cout << "Bienvenido a nuestra biblioteca pulse:\n" << endl;
-        cout << "1.- Contrase馻 de admin. " << endl;
-        cout << "2.- Registrarse como usuario. " << endl;
-        cout << "0.- Salir. " << endl;
-        cin >> opcion;
-        switch (opcion) {
-            case 1:
-            {
-                aplica.aplicacion_admin();
-            }
-                break;
-            case 2:
-            {
-                aplica.aplicacion_usuario();
-            }
-                break;
-        }
-    } while (opcion != 0);
-    system("pause");
-    system("cls");
-    return 0;
+	unsigned opcion;
+	Aplication aplica;
+	do {
+		cout << "Bienvenido a nuestra biblioteca pulse:\n" << endl;
+		cout << "1.- Contrase锟絘 de admin. " << endl;
+		cout << "2.- Registrarse como usuario. " << endl;
+		cout << "0.- Salir. " << endl;
+		cin >> opcion;
+		switch (opcion) {
+		case 1: {
+			aplica.aplicacion_admin();
+		}
+			break;
+		case 2: {
+			aplica.aplicacion_usuario();
+		}
+			break;
+		}
+	} while (opcion != 0);
+	system("pause");
+	system("cls");
+	return 0;
 }
