5e9d75406053da4a95ca1185e78d1b4c6e17dafa
Andrés Rojas Ortega <aro00015@red.ujaen.es>
2021-03-02 20:11:48 +0100

    Ususario.h: Añadidas etiquetas doxygen (COMPLETAR)

--- Usuario.h
+++ Usuario.h
@@ -1,3 +1,8 @@
+/**
+ * @file Usuario.h
+ * @brief Archivo cabecera donde se almacena la información de la clase Ususario
+ */
+
 #ifndef USUARIO_H
 #define	USUARIO_H
 
@@ -5,36 +10,100 @@
 #include "lista_sin.h"
 using namespace std;
 
+/**
+ * @brief
+ */
 class Usuario {
-    string nombre, clave, login;
+	string nombre; ///<
+	string clave; ///<
+	string login; ///<
 public:
-    Usuario();
-    void rellena(string anombre, string alogin, string aclave);
-    bool validarClave(string claven);
-    void cambiarClave(string nuevaClave);
-    string daClave() {
-        return clave;
-    }
-    string daLogin() {
-        return login;
-    }
-    string daLogin(Usuario *usuario) {
-        return login;
-    }
-    Usuario& operator=(const Usuario &usu) {
-        this->nombre = usu.nombre;
-        this->clave = usu.clave;
-        this->login = usu.login;
-        return *this;
-    }
-    friend ostream& operator<<(ostream&, const Usuario&);
-    bool operator<(const Usuario &usu) {
-        return (this->nombre < usu.nombre);
-    }
-    ~Usuario();
+
+	/**
+	 * @brief Constructor por defecto de la clase Ususario
+	 */
+	Usuario();
+
+	/**
+	 * @brief
+	 * @param [in] anombre string.
+	 * @param [in] alogin string.
+	 * @param [in] aclave string.
+	 */
+	void rellena(string anombre, string alogin, string aclave);
+
+	/**
+	 * @brief
+	 * @param [in] claven string.
+	 * @return
+	 */
+	bool validarClave(string claven);
+
+	/**
+	 * @brief
+	 * @param [in] nuevaClave string.
+	 */
+	void cambiarClave(string nuevaClave);
+
+	/**
+	 * @brief Método getter del atributo clave
+	 * @return clave string. Atributo de la clase
+	 */
+	string daClave() {
+		return clave;
+	}
+
+	/**
+	 * @brief Método getter del atributo login
+	 * @return login string. Atributo de la clase
+	 */
+	string daLogin() {
+		return login;
+	}
+
+	/**
+	 * @brief
+	 * @param [in] usuario Ususario (ref)
+	 * @return
+	 */
+	string daLogin(Usuario *usuario) {
+		return login;
+	}
+
+	/**
+	 * @brief Operador de asignación de la clase Usuario.
+	 * @param [in] usu Ususario (const, dir).
+	 * @return Instancia copia de la clase Usuario creada
+	 */
+	Usuario& operator=(const Usuario &usu) {
+		this->nombre = usu.nombre;
+		this->clave = usu.clave;
+		this->login = usu.login;
+		return *this;
+	}
+
+	/**
+	 * @brief
+	 * @param
+	 * @param
+	 * @return
+	 */
+	friend ostream& operator<<(ostream&, const Usuario&);
+
+	/**
+	 * @brief Operador menor que de la clase Usuario
+	 * @param [in] usu Usuario (dir)
+	 * @return true si el nombre de usu es mayor, false en cualquier otro caso
+	 */
+	bool operator<(const Usuario &usu) {
+		return (this->nombre < usu.nombre);
+	}
+
+	/**
+	 * @brief Destructor de la clase Ususario
+	 */
+	~Usuario();
 };
-
-
 
 #endif	/* USUARIO_H */
 
