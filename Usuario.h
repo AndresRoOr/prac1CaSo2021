58f9c2d049f0506f291042c4e695546c652efbeb
Esteban Jodar Pozo <ejp00003@red.ujaen.es>
2021-03-04 22:14:43 +0100

    Lo mismo pero con el punto h de Usuario

--- Usuario.h
+++ Usuario.h
@@ -1,6 +1,6 @@
 /**
  * @file Usuario.h
- * @brief Archivo cabecera donde se almacena la información de la clase Ususario
+ * @brief Archivo cabecera donde se almacena la informacion de la clase Ususario.
  */
 
 #ifndef USUARIO_H
@@ -11,69 +11,53 @@
 using namespace std;
 
 /**
- * @brief
+ * @brief Clase que representa a un usuario de la biblioteca.
  */
 class Usuario {
-	string nombre; ///<
-	string clave; ///<
-	string login; ///<
+	string nombre;   ///< Nombre del usuario.
+	string clave;    ///< Clave que lo autenticara ante el sistema.
+	string login;    ///< Login del usuario.
 public:
 
 	/**
-	 * @brief Constructor por defecto de la clase Ususario
+	 * @brief Constructor por defecto de la clase Ususario.
 	 */
 	Usuario();
 
 	/**
-	 * @brief
-	 * @param [in] anombre string.
-	 * @param [in] alogin string.
-	 * @param [in] aclave string.
+	 * @brief Funcion que asigna a un usuario su nombre, su clave, y por supuesto su login.
 	 */
 	void rellena(string anombre, string alogin, string aclave);
 
 	/**
-	 * @brief Devuelve un booleano para saber si existe la clave.
-	 * @param [in] claven string.
-	 * @return True si existe la clave, false en cualquier otro caso
+	 * @brief Comprueba si existe la clave.
 	 */
 	bool validarClave(string claven);
 
 	/**
-	 * @brief Cambia el atributo clave con el valor pasado como parámetro
-	 * @param [in] nuevaClave string.
+	 * @brief Cambia el atributo clave con el string pasado como parametro.
 	 */
 	void cambiarClave(string nuevaClave);
 
 	/**
-	 * @brief Método getter del atributo clave
-	 * @return clave string. Atributo de la clase
+	 * @brief Metodo getter del atributo clave.
 	 */
-	string daClave() {
-		return clave;
-	}
+	string daClave();
 
 	/**
-	 * @brief Método getter del atributo login
-	 * @return login string. Atributo de la clase
+	 * @brief Metodo getter del atributo login.
 	 */
-	string daLogin() {
-		return login;
-	}
+	string daLogin();
 
 	/**
-	 * @brief
-	 * @param [in] usuario Ususario (ref)
-	 * @return
+	 * @brief Metodo para loguear a un usuario con nombre y clave corectos.
 	 */
-	string daLogin(Usuario *usuario) {
-		return login;
-	}
+	string daLogin(Usuario *usuario);
 
 	/**
-	 * @brief Operador de asignación de la clase Usuario.
-	 * @param [in] usu Ususario (const, dir).
-	 * @return Instancia copia de la clase Usuario creada
+	 * @brief Operador de asignacion de la clase Usuario.
+	 * @param [in] usu Usuario (const, dir).
+	 * @return Instancia copia de la clase Usuario creada.
 	 */
 	Usuario& operator=(const Usuario &usu) {
 		this->nombre = usu.nombre;
@@ -83,24 +67,21 @@ public:
 	}
 
 	/**
-	 * @brief
-	 * @param
-	 * @param
-	 * @return
+	 * @brief Sobrecarga del operador << (de insercion) que permite entre otras cosas mostrar una fecha con cout.
 	 */
 	friend ostream& operator<<(ostream&, const Usuario&);
 
 	/**
-	 * @brief Operador menor que de la clase Usuario
-	 * @param [in] usu Usuario (dir)
-	 * @return true si el nombre de usu es mayor, false en cualquier otro caso
+	 * @brief Operador "menor que" de la clase Usuario.
+	 * @param [in] usu Usuario (dir).
+	 * @return true si el nombre de usu es mayor, false en cualquier otro caso.
 	 */
 	bool operator<(const Usuario &usu) {
 		return (this->nombre < usu.nombre);
 	}
 
 	/**
-	 * @brief Destructor de la clase Ususario
+	 * @brief Destructor de la clase Usuario.
 	 */
 	~Usuario();
 };
