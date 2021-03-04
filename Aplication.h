395d5d131acd72820d461ec4f249e902503b037c
Esteban Jodar Pozo <ejp00003@red.ujaen.es>
2021-03-04 22:09:00 +0100

    Arreglando el punto h como decia Nacho

--- Aplication.h
+++ Aplication.h
@@ -14,15 +14,15 @@ using namespace std;
  * @brief Clase principal la cual derivara en sus variantes de admin y de usuario.
  */
 class Aplication {
-	Biblioteca bi; ///< Objeto de la clase Biblioteca.
-	Usuario *usu; ///< Referencia a un usuario en concreto.
-	lista_sin<Usuario> lusu; ///< Lista de los usuarios registrados en la biblioteca.
-	Libro li; ///< Objeto de la clase libro.
-	PedidoBiblioteca *pedBi; ///< Referencia a un pedido especifico hecho por la biblioteca
-	lista_sin<PedidoBiblioteca *> * pedbi; ///< Lista de todos los libros pedidos en cada solicitud.
-	lista_sin<PedidoUsuario *> * pedusu; ///< Lista de los pedidos hechos por los usuarios.
-	lista_sin<Libro *> * libro; ///< Lista de todos los libros pedidos por todos los usuarios.
-	PedidoBiblioteca *pedbipunt; ///< Lista de los pedidos de la biblioteca no tramitados.
+	Biblioteca bi;                          ///< Objeto de la clase Biblioteca.
+	Usuario *usu;                           ///< Referencia a un usuario en concreto.
+	lista_sin<Usuario> lusu;                ///< Lista de los usuarios registrados en la biblioteca.
+	Libro li;                               ///< Objeto de la clase libro.
+	PedidoBiblioteca *pedBi;                ///< Referencia a un pedido especifico hecho por la biblioteca
+	lista_sin<PedidoBiblioteca *> * pedbi;  ///< Lista de todos los libros pedidos en cada solicitud.
+	lista_sin<PedidoUsuario *> * pedusu;    ///< Lista de los pedidos hechos por los usuarios.
+	lista_sin<Libro *> * libro;             ///< Lista de todos los libros pedidos por todos los usuarios.
+	PedidoBiblioteca *pedbipunt;            ///< Lista de los pedidos de la biblioteca no tramitados.
 public:
 
 	/**
@@ -36,7 +36,7 @@ public:
 	void aplicacion_usuario();
 
 	/**
-	 * @brief Da la entrada al esquema de usuario.
+	 * @brief Da la entrada al esquema de administrador.
 	 */
 	void aplicacion_admin();
 
