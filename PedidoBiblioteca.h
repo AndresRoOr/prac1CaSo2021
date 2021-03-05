c28ba9ad30a02e53b1ae20c5ffa82fe2c5f83cef
Esteban Jodar Pozo <ejp00003@red.ujaen.es>
2021-03-05 23:05:45 +0100

    PedidoBiblioteca.h creo que ya esta

--- PedidoBiblioteca.h
+++ PedidoBiblioteca.h
@@ -14,10 +14,10 @@
  * @brief Clase que representa de manera generica un pedido hecho por la biblioteca.
  */
 class PedidoBiblioteca {
-	Fecha fecha; ///< Queda registrada la fecha del pedido actualizada con la fecha y hora del sistema.
-	float importe;  ///< Importe total de todos los usuarios.
-	bool tramitado; ///< Booleano a true si el pedido esta tramitado, false en otro caso.
-	unsigned num;   ///< Numero de pedido de biblioteca.
+	Fecha fecha;                          ///< Queda registrada la fecha del pedido actualizada con la fecha y hora del sistema.
+	float importe;                        ///< Importe total de todos los usuarios.
+	bool tramitado;                       ///< Booleano a true si el pedido esta tramitado, false en otro caso.
+	unsigned num;                         ///< Numero de pedido de biblioteca.
 	lista_sin<PedidoUsuario *> pedido_usu; ///< Registro en la estructura de datos del pedido de un usuario.
 
 public:
@@ -32,7 +32,7 @@ public:
 	 * @param [in] anum unsigned.
 	 */
 	PedidoBiblioteca(unsigned anum) :
-			fecha() {
+		fecha() {
 		importe = 0;
 		tramitado = false;
 		this->pedido_usu = pedido_usu;
@@ -53,58 +53,41 @@ public:
 
 	/**
 	 * @brief Se inserta el pedido de un usuario a un libro en concreto en la estructura de datos.
-	 * @param [in] pedidoUsuario.
-	 * @return Nada.
 	 */
 	void insertaPedidoLibro(PedidoUsuario *pedidoUsuario);
 
 	/**
 	 * @brief Da por concluido el tramite de hacer el pedido.
-	 * @param [in] Nada.
-	 * @return Nada.
 	 */
 	void cierraPedido();
 
 	/**
 	 * @brief Metodo getter del atributo tramitado.
-	 * @return tramitado bool. Atributo de la clase.
 	 */
-	bool daTrami() {
-		return tramitado;
-	}
+	bool daTrami();
 
 	/**
 	 * @brief Metodo getter del atributo num.
-	 * @return num unsigned. Atributo de la clase.
 	 */
-	unsigned daNumero() {
-		return num;
-	}
+	unsigned daNumero();
 
 	/**
 	 * @brief Metodo getter del atributo importe.
-	 * @return importe float. Atributo de la clase.
 	 */
-	float daImporte() {
-		return importe;
-	}
+	float daImporte();
 
 	/**
-	 * @brief Sobrecarga del operador << (de insercion) que permite entre otras cosas mostrar una fecha con cout.
+	 * @brief Funcion necesaria para la clase Biblioteca en busqueda de pedidos.
+	 */
+	PedidoUsuario* daPedidoUsuario(unsigned i);
+
+	/**
+	 * @brief Sobrecarga del operador << (de insercion) que permite entre otras cosas mostrar un PedidoBiblioteca con cout.
 	 * @param [in] ostream. El propio flujo de salida (por referencia).
 	 * @param [in] PedidoBiblioteca. El pedido de la biblioteca (por referencia tambien).
 	 * @return Nada.
 	 */
 	friend ostream& operator<<(ostream&, PedidoBiblioteca&);
-
-	/**
-	 * @brief Funcion necesaria para la clase Biblioteca en busqueda de pedidos.
-	 * @param [in] i unsigned. El usuario apuntado por i.
-	 * @return pedido_usu_lee PedidoUsuario*. El pedido que ha hecho el usuario referenciado por i en la estructura de datos.
-	 */
-	PedidoUsuario* daPedidoUsuario(unsigned i) {
-		return pedido_usu.lee(i);
-	}
 
 	/**
 	 * @brief Operador de asignacion de la clase PedidoBiblioteca.
