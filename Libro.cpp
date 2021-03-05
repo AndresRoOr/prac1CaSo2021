df5d3692be39702f9fea593332491ddbd41b03f9
Esteban Jodar Pozo <ejp00003@red.ujaen.es>
2021-03-05 21:25:49 +0100

    Terminado Libro.cpp

--- Libro.cpp
+++ Libro.cpp
@@ -1,10 +1,15 @@
 /**
  * @file Libro.cpp
- * @brief Contiene el c√≥digo fuente de todas las funciones del archivo cabecera Libro.h
+ * @brief Contiene el codigo fuente de todas las funciones del archivo cabecera Libro.h
  */
 
 #include "Libro.h"
 
+
+/**
+ * @brief Constructor por defecto de la clase Libro.
+ * @pre Al estar inicializando un objeto de la clase, todos los atributos aparecen vacios o inicializados a cero.
+ */
 Libro::Libro() {
 	titulo = "";
 	autores = "";
@@ -14,9 +19,16 @@ Libro::Libro() {
 	anio = 0;
 }
 
-Libro::Libro(string aTitulo, string aAutores, string aEditorial, string aISBN,
-		int aAnio, float aPrecioActual) {
-
+/**
+ * @brief Constructor parametrizado de la clase Libro.
+ * @param [in] aTitulo string. Titulo del libro.
+ * @param [in] aAutores string. Autor/es del libro.
+ * @param [in] aEditorial string. Editorial del libro.
+ * @param [in] aISBN string. ISBN del libro.
+ * @param [in] aAnio int. AÒo de publicacion del libro.
+ * @param [in] aPrecioActual float. Precio del libro.
+ */
+Libro::Libro(string aTitulo, string aAutores, string aEditorial, string aISBN, int aAnio, float aPrecioActual) {
 	titulo = aTitulo;
 	autores = aAutores;
 	editorial = aEditorial;
@@ -25,13 +37,54 @@ Libro::Libro(string aTitulo, string aAutores, string aEditorial, string aISBN,
 	precioActual = aPrecioActual;
 }
 
+/**
+ * @brief Sobrecarga del operador << (de insercion) que permite entre otras cosas, mostrar un libro con cout.
+ * @param [in]  co ostream. El propio flujo de salida (por referencia).
+ * @param [in] Libro. Un libro en concreto con su titulo, ISBN, Editorial y autor/es (por referencia tambien).
+ * @return [out] co. El flujo de salida ostream debidamente sobrecargado.
+ */
 ostream& operator<<(ostream& co, const Libro& l) {
 	co << "Titulo: " << l.titulo << endl << "ISBN: " << l.ISBN << endl
-			<< "Editorial: " << l.editorial << endl << "Autores: " << l.autores
-			<< endl;
+			<< "Editorial: " << l.editorial << endl << "Autores: " << l.autores << endl;
 	return co;
 }
 
+/**
+ * @brief Metodo getter del atributo ISBN.
+ * @return ISBN string. El International Standard Book Number del libro.
+ */
+	string Libro::daISBN() {
+		return ISBN;
+	}
+
+/**
+ * @brief Metodo getter del atributo titulo con parametro.
+ * @param [in] libro Libro(ref).
+ * @return titulo string. El titulo del libro referenciado.
+ */
+	string Libro::daTitulo(Libro *libro) {
+			return titulo;
+	}
+
+/**
+ * @brief Metodo getter del atributo titulo.
+ * @return titulo string. El titulo del libro.
+ */
+	string Libro::daTitulo() {
+		return titulo;
+	}
+
+/**
+ * @brief Metodo getter del atributo precio.
+ * @return precioActual float. El coste de la adquisicion del libro.
+ */
+	float Libro::daPrecioActual() {
+		return precioActual;
+	}
+
+/**
+ * @brief Destructor de la clase Libro.
+ */
 Libro::~Libro() {
 }
 
